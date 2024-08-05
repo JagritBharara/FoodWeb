d155c7a949aac4a90a5c22e0f559225a2e9fddca
Saksham <sakshamnegi40069@gmail.com>
2024-08-05 16:29:28 +0530

    fixed a few things, fixed the explore menu full content not visible, fixed some erros

--- frontEnd/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontEnd/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
-import './ExploreMenu.css'
-import { menu_list } from '../../assets/assets'
-const ExploreMenu = ({category,setCategory}) => {
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
+const ExploreMenu = ({ category, setCategory }) => {
   return (
-    <div className='explore-menu' id='explore-menu'>
-        <h1>Explore Our Menu</h1>
-        <p className='explore-menu-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ducimus blanditiis eos mollitia voluptatibus similique animi tempora qui illo delectus.</p>
-        <div className="explore-menu-list">
-            {menu_list.map((item,index)=>{
-                return (
-                    <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
-                        <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
-                        <p>{item.menu_name}</p>
-                    </div>
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore Our Menu</h1>
+      <p className="explore-menu-text">
+        Welcome to a culinary journey like no other. Our menu is a celebration
+        of flavors, crafted with passion and served with love. From gourmet
+        delicacies to hearty favorites, every dish is designed to tantalize your
+        taste buds. Here’s a glimpse of what awaits you.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
                 )
-            })}
-        </div>
-        <hr />
+              }
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+                alt=""
+              />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default ExploreMenu
+export default ExploreMenu;
